e9c17cafa96bc231a23f5c0603edefaf66690522
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2024-01-18 03:07:34 -0500

    bug fix

--- genshin-start.js
+++ genshin-start.js
@@ -1,5 +1,9 @@
 window.onload = function() {
     setTimeout(function() {
         document.getElementById('genshin').style.opacity = "1";
+        var links = document.getElementsByTagName('a');
+        for (var i = 0; i < links.length; i++) {
+            links[i].target = '_blank';
+        }
     } );
 };
